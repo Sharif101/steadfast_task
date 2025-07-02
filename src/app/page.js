5e42eefebf816eb4ff27567bb5e8bf9453fb1802
Sharif101 <sharif009009@gmail.com>
2025-07-02 12:06:52 +0600

    add

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,9 @@
-"use client";
 import Landing from "@/component/Landing/Landing";
 import { handleFetch } from "@/hooks/useRequest";
-import { useEffect, useState } from "react";
 
-export default function Home() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    handleFetch("shop/products").then((res) => {
-      console.log("API response:", res);
-
-      if (res && res.data && res.data.products) {
-        setProducts(res.data.products);
-      } else if (res) {
-        setProducts(res);
-      }
-    });
-  }, []);
-
-  console.log({ products });
+export default async function Home() {
+  const res = await handleFetch("shop/products");
+  const products = res?.data?.products || res || [];
 
   return (
     <div>
