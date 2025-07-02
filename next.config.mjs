5967da0b4c182bf6857275bf9b9febce5379f8ae
Sharif101 <sharif009009@gmail.com>
2025-07-02 12:09:42 +0600

    add

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,7 @@
-// next.config.mjs
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "157.230.240.97",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "via.placeholder.com",
-        pathname: "/**",
-      },
-    ],
+    domains: ["157.230.240.97", "https://via.placeholder.com"],
   },
 };
 
