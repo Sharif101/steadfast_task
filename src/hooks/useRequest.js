26820ebd198d3c7009f2221fe4d7103f42decf0b
Sharif101 <sharif009009@gmail.com>
2025-07-02 11:48:43 +0600

    add

--- src/hooks/useRequest.js
+++ src/hooks/useRequest.js
@@ -6,16 +6,13 @@ export const handleFetch = async (endPoint, params = {}) => {
       ? "?" + new URLSearchParams(params).toString()
       : "";
 
-    const response = await fetch(
-      `http://157.230.240.97:9999/api/v1/${endPoint}${queryString}`,
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-        cache: "no-store",
-      }
-    );
+    const response = await fetch(`${api}/${endPoint}${queryString}`, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      cache: "no-store",
+    });
 
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
