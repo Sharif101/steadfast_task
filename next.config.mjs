ff61db8456c20ba4a899fca0c2f597c97ba5c07f
Sharif101 <sharif009009@gmail.com>
2025-07-02 16:26:38 +0600

    change url

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["157.230.240.97", "https://via.placeholder.com"],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "157.230.240.97",
+        port: "8888", // ✅ Port must be specified if not 80/443
+        pathname: "/storage/media/**", // ✅ Match image path
+      },
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
